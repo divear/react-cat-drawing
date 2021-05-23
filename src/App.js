ed763edc1568fb141770ffb06ecd8629c7925ae3
Lukas <lukyn.odehnal333@gmail.com>
2021-05-23 15:09:06 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import React,{useState,useEffect, useRef} from 'react'
 import './App.css';
 
 function App() {
+  var Xpos;
+  var Ypos;
+
+  const [image,setImage] = useState();
+  onmousemove = function(e){
+    Xpos = e.clientX 
+    Ypos = e.clientY
+  }
+
+  const canvas = useRef()
+
+  useEffect(()=>{
+    const image = new Image();
+    image.src = "https://thiscatdoesnotexist.com/"
+    image.onload = () => setImage(image)
+  },[])
+
+  useEffect(()=>{
+    if(image && canvas){
+      const c = canvas.current.getContext("2d")
+      c.fillStyle = 'black'
+      c.fillRect(0, 0, 650, 550);
+      c.drawImage(image, (400 - 256)/2, 0);
+    }
+  },[image, canvas]);
+
+  function Draw(){
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    const c = canvas.current.getContext("2d")
+    c.strokeStyle = "teal"
+    c.lineWidth = 10
+    c.beginPath();
+    c.arc(Xpos,Ypos,1,0,2*Math.PI);
+    c.stroke();
+    c.fillStyle = color
+    //c.fillRect(Math.random()*1000, Math.random()*1000, Math.random()*1000, Math.random()*1000);
+    c.drawImage(image, Math.random()*1000, Math.random()*1000, Math.random()*1000, Math.random()*1000)
+
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <canvas
+        onMouseMoveCapture={Draw}
+        ref={canvas}
+        width={window.innerWidth - 50}
+        height={window.innerHeight -50}
+        />
+      </div>
+      <div>
+        
+      </div>
     </div>
   );
 }
